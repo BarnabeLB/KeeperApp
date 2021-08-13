ba74f1c2bfe5c4d43f85ae62aeafe89455435826
BarnabeLB <lebelbarnabe@gmail.com>
2021-08-13 19:31:51 +0200

    Index.js with imports from different components

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return(
+        <div className ="note">
+            <h1>First Title</h1>
+            <p>First note content ! </p>
+        </div>
+    );
+}
+
+export default Note;
