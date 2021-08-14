b3f1f35afeb7aa2e37dfcf775d630167207dd1c9
BarnabeLB <lebelbarnabe@gmail.com>
2021-08-14 17:50:03 +0200

    rendering notes using .map() method and props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
+      {notes.map((note) => (                                                  // .map() crééer un array à partir d'élements d'un autre array notes[], chaque index de l'array prendra le nom (note)
+        <Note key={note.key} title={note.title} content={note.content} />     // on récupère les data voulu et on les envoit au props. On réitière jusquà la fin du tableau
+      ))}
       <Footer />
-      <Note />
     </div>
   );
 }
